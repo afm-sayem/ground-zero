b7d71cfd80f516df108350a5b4072d7c3b5ef576
Sayem <afm.sayem@gmail.com>
2016-07-01 10:48:56 +0600

    A person can be in several movies

--- server/api/person/person.model.js
+++ server/api/person/person.model.js
@@ -1,6 +1,5 @@
-'use strict';
-
 const Model = require('objection').Model;
+const path = require('path');
 
 class Person extends Model {
   static get tableName() {
@@ -15,6 +14,24 @@ class Person extends Model {
       properties: {
         id: {type: 'integer'},
         name: {type: 'string', minLength: 1, maxLength: 255}
+      }
+    };
+  }
+
+  static get relationshipMappings () {
+    return {
+      movies: {
+        relation: Model.ManyToManyRelation,
+        modelClass: path.normalize(__dirname + '/../movie/movie.model'),
+        join: {
+          from: 'Person.id',
+          through: {
+            from: 'Person_Movie.personId',
+            to: 'Person_Movie.movieId',
+            extra: ['character']
+          },
+          to: 'Movie.id'
+        }
       }
     };
   }
