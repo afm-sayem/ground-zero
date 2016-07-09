6e10000db3de4f04906fbc204a8e76a343f0755f
Sayem <afm.sayem@gmail.com>
2016-07-09 21:56:24 +0600

    Expose authentication api

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,11 +1,17 @@
 'use strict';
 
 const express = require('express');
-const authUtils = require('./authutils');
 const facebook = require('./facebook');
+const local = require('./auth.controller');
 
 const router = express.Router();
 
 router.post('/facebook', facebook.authenticate);
+
+router.post('/signup', local.signup);
+router.get('/verify', local.verify);
+router.post('/login', local.login);
+router.post('/request-reset', local.requestReset);
+router.post('/reset', local.reset);
 
 module.exports = router;
