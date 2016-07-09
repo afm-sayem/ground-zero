b2cda10e8d24141fd1b984f622b705529c59a6a1
Sayem <afm.sayem@gmail.com>
2016-07-09 21:51:41 +0600

    Use connection string instead to connect to redis

--- server/config/redis.js
+++ server/config/redis.js
@@ -1,18 +1,9 @@
 const Promise = require('bluebird');
 const redis = require('redis');
-const url = require('url');
 Promise.promisifyAll(redis.RedisClient.prototype);
 Promise.promisifyAll(redis.Multi.prototype);
 
-const config = require('../config/environment');
-
-const redisConfig = url.parse(config.redisUrl);
-const client = redis.createClient(redisConfig.port, redisConfig.hostname);
-
-if (redisConfig.auth !== null) {
-  client.auth(redisConfig.auth.split(':')[1]);
-}
-
+const client = redis.createClient(process.env.REDIS_URL);
 client.on('error', (e) => e);
 
 module.exports = client;
