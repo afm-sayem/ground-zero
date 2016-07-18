38a6603dd38f3dcdbd1722a7715d09b67cde1240
Sayem <afm.sayem@gmail.com>
2016-07-19 01:58:06 +0600

    Added airbnb javascript style linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,26 +5,8 @@ module.exports = {
     "node": true
   },
   "parser": "babel-eslint",
-  "extends": "eslint:recommended",
+  "extends": "airbnb-base",
   "parserOptions": {
     "sourceType": "module"
-  },
-  "rules": {
-    "indent": [
-      "error",
-      2
-    ],
-    "linebreak-style": [
-      "error",
-      "unix"
-    ],
-    "quotes": [
-      "error",
-      "single"
-    ],
-    "semi": [
-      "error",
-      "always"
-    ]
   }
 };
