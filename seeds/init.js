c76440a0eb4bf868525386b766774664c9009f49
Sayem <afm.sayem@gmail.com>
2016-07-20 09:12:21 +0600

    Removes using explicit id from create statement

--- seeds/init.js
+++ seeds/init.js
@@ -6,20 +6,20 @@ exports.seed = function(knex, Promise) {
     knex('Person').del()
   ]))
   .then(() => Promise.all([
-    knex('Person').insert({id: 1, name: 'Satyajit Ray'}),
-    knex('Person').insert({id: 2, name: 'Subir Banarjee'}),
-    knex('Person').insert({id: 3, name: 'Kanu Banarjee'}),
-    knex('Person').insert({id: 4, name: 'Ritwik Ghatak'}),
-    knex('Person').insert({id: 5, name: 'Kali Banarjee'}),
+    knex('Person').insert({name: 'Satyajit Ray'}),
+    knex('Person').insert({name: 'Subir Banarjee'}),
+    knex('Person').insert({name: 'Kanu Banarjee'}),
+    knex('Person').insert({name: 'Ritwik Ghatak'}),
+    knex('Person').insert({name: 'Kali Banarjee'}),
 
-    knex('Type').insert({id: 1, name: 'Drama'}),
-    knex('Type').insert({id: 2, name: 'Comedy'})
+    knex('Type').insert({name: 'Drama'}),
+    knex('Type').insert({name: 'Comedy'})
   ]))
   .then(() => Promise.all([ 
-    knex('Movie').insert({id: 1, title: 'Pather Panchali', poster: 'pather_panchali.jpg', directorId: 1, typeId: 1}),
-    knex('Movie').insert({id: 2, title: 'Aparijito', poster: 'aparajito.jpg', directorId: 1, typeId: 1}),
-    knex('Movie').insert({id: 3, title: 'Goopy Gyne Bagha Byne', poster: 'goopy_bagha.jpg', directorId: 1, typeId: 2}),
-    knex('Movie').insert({id: 4, title: 'Meghe Dhaka Tara', poster: 'meghe_dhaka.jpg', directorId: 2, typeId: 1}),
-    knex('Movie').insert({id: 5, title: 'Ajantrik', poster: 'ajantrik.jpg', directorId: 2, typeId: 2})
+    knex('Movie').insert({title: 'Pather Panchali', poster: 'pather_panchali.jpg', directorId: 1, typeId: 1}),
+    knex('Movie').insert({title: 'Aparijito', poster: 'aparajito.jpg', directorId: 1, typeId: 1}),
+    knex('Movie').insert({title: 'Goopy Gyne Bagha Byne', poster: 'goopy_bagha.jpg', directorId: 1, typeId: 2}),
+    knex('Movie').insert({title: 'Meghe Dhaka Tara', poster: 'meghe_dhaka.jpg', directorId: 2, typeId: 1}),
+    knex('Movie').insert({title: 'Ajantrik', poster: 'ajantrik.jpg', directorId: 2, typeId: 2})
   ]));
 };
