e710fb9d631bdb4a409ab40b5ecbcfcd687d55d6
Sayem <afm.sayem@gmail.com>
2016-07-21 12:46:57 +0600

    Deletes hardcoded database connection details

--- knexfile.js
+++ knexfile.js
@@ -1,21 +1,14 @@
 module.exports = {
-
   development: {
     client: 'postgresql',
-    connection: {
-      host     : '127.0.0.1',
-      user     : 'kishan',
-      password : 'jmjm',
-      database : 'seed',
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 0,
-      max: 7
-    }
+      max: 7,
+    },
   },
-
   production: {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL
-  }
+    connection: process.env.DATABASE_URL,
+  },
 };
