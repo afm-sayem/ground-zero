8fd63415c844f6bc7e640e21539047acc7b891da
Sayem <afm.sayem@gmail.com>
2016-07-29 21:06:51 +0600

    Add cors support

--- app.js
+++ app.js
@@ -1,25 +1,23 @@
 require('dotenv').config();
-const path = require('path');
-if (process.env.NODE_ENV === 'production') {
-  process.env.NEW_RELIC_HOME = path.resolve(__dirname(), '/config/newrilc.js');
-  require('newrelic');
-}
+if (process.env.NODE_ENV === 'production') require('newrelic');
 
 const morgan = require('morgan');
 const compress = require('compression');
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const knexConfig = require('./knexfile');
 const registerApi = require('./routes');
 const Model = require('objection').Model;
-
 const knex = require('knex')(knexConfig[process.env.NODE_ENV]);
+
 Model.knex(knex);
 
 const app = express()
   .use(bodyParser.json())
   .use(morgan('dev'))
   .use(compress())
+  .use(cors())
   .set('json spaces', 2);
 
 registerApi(app);
