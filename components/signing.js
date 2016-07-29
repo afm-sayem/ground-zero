f2fbfea116555765fb6f72b263deb64332fb1450
Sayem <afm.sayem@gmail.com>
2016-07-29 20:59:12 +0600

    Refactor components

--- components/signing.js
+++ components/signing.js
@@ -1,31 +1,28 @@
-'use strict';
 const aws = require('aws-sdk');
 const config = require('../config/environment');
 
-module.exports = function(req, res) {
-  aws.config.update({accessKeyId: config.aws.access, secretAccessKey: config.aws.secret });
-  aws.config.update({region: config.aws.region, signatureVersion: 'v4' });
-  let s3 = new aws.S3(); 
-  let file_name = `${(new Date()).getTime()}_${req.query.file_name}`;
-  let s3_params = { 
-    Bucket: config.aws.bucket, 
-    Key: `${req.query.folder_name}/${file_name}`, 
-    Expires: 60, 
-    ContentType: req.query.file_type, 
-    ACL: 'public-read'
-  }; 
+module.exports = function (req, res) {
+  aws.config.update({ accessKeyId: config.aws.access, secretAccessKey: config.aws.secret });
+  aws.config.update({ region: config.aws.region, signatureVersion: 'v4' });
+  const s3 = new aws.S3();
+  const fileName = `${(new Date()).getTime()}_${req.query.fileName}`;
+  const params = {
+    Bucket: config.aws.bucket,
+    Key: `${req.query.folder_name}/${fileName}`,
+    Expires: 60,
+    ContentType: req.query.file_type,
+    ACL: 'public-read',
+  };
 
-  s3.getSignedUrl('putObject', s3_params, function(err, data) { 
-    if (err) { 
-      return res.status(400).send({err: err});
+  s3.getSignedUrl('putObject', params, (err, data) => {
+    if (err) {
+      return res.status(400).send({ err });
     }
-    else { 
-      let return_data = {
-        signed_request: data,
-        url: file_name
-      };
-      res.send(return_data);
-    } 
+    const response = {
+      signed_request: data,
+      url: fileName,
+    };
+    return res.send(response);
   });
 };
 
