852d85f87e10a927c343cf468c04b8ad2a24dc14
Sayem <afm.sayem@gmail.com>
2016-08-12 21:53:54 +0600

    Use integer instead of big integer

--- migrations/20160628173100_person_movie.js
+++ migrations/20160628173100_person_movie.js
@@ -1,10 +1,10 @@
 exports.up = function (knex) {
   return knex.schema
     .createTable('person_movie', (table) => {
-      table.bigincrements('id').primary();
-      table.biginteger('person_id').unsigned().references('id').inTable('person')
+      table.increments('id').primary();
+      table.integer('person_id').unsigned().references('id').inTable('person')
         .onDelete('CASCADE');
-      table.biginteger('movie_id').unsigned().references('id').inTable('movie')
+      table.integer('movie_id').unsigned().references('id').inTable('movie')
         .onDelete('CASCADE');
       table.string('character');
     });
--- migrations/20160730073714_reviews.js
+++ migrations/20160730073714_reviews.js
@@ -2,7 +2,7 @@ exports.up = function (knex) {
   return knex.schema
     .createTable('review', (table) => {
       table.increments('id').primary();
-      table.biginteger('movie_id').unsigned().references('id').inTable('movie')
+      table.integer('movie_id').unsigned().references('id').inTable('movie')
         .onDelete('CASCADE');
       table.string('title');
       table.text('description');
