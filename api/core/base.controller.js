1f0e606cfd0ed4be4e7b9c8c402f8fa86fea3968
Sayem <afm.sayem@gmail.com>
2016-11-17 17:12:31 +0600

    refactor(base): add methods for validating nested resources

--- api/core/base.controller.js
+++ api/core/base.controller.js
@@ -1,17 +1,70 @@
 const findQuery = require('objection-find');
 const searchFilter = require('../../components/filters/text-search');
 const utilities = require('../../components/utilities');
+const Promise = require('bluebird');
+
+function getParameterFilters(req, filterProperties) {
+  let paramFilter;
+  if (filterProperties) {
+    paramFilter = {};
+    filterProperties.forEach((item) => {
+      paramFilter[item.prop] = Number(req.params[item.prop]);
+    });
+  }
+  return paramFilter;
+}
+
+async function isValidData(req, additionalProperties) {
+  const data = additionalProperties.filter(value => value.checkExistence);
+  try {
+    const props = await Promise.map(data, (item) => {
+      return item.model.query()
+        .findById(Number(req.params[item.prop]));
+    });
+    if (props.some(item => item === undefined || item === null)) {
+      return false;
+    }
+    return true;
+  } catch (e) {
+    /* handle error */
+    return new Error('Error in looking up resources');
+  }
+}
+
+function getAdditionalProperties(req, data) {
+  const additionalProperties = {};
+  data
+    .filter(item => item.include)
+    .forEach((item) => {
+      additionalProperties[item.prop] = Number(req.params[item.prop]);
+    });
+  return additionalProperties;
+}
 
 class BaseController {
-  constructor(model, id = 'id', eager) {
+  constructor(model, id, additionalProperties, userField) {
     this.model = model;
-    this.eager = eager;
     this.id = id;
+    this.additionalProperties = additionalProperties;
+    this.userField = userField;
   }
 
-  create(req, res) {
+  async create(req, res) {
+    let data = req.body;
+    if (this.additionalProperties) {
+      if (await isValidData(req, this.additionalProperties)) {
+        data = Object.assign({}, data, getAdditionalProperties(req, this.additionalProperties));
+      } else {
+        return utilities.throwNotFound(res);
+      }
+    }
+
+    if (this.userField) {
+      data[this.userField] = req.user.id;
+    }
+
     return this.model.query()
-      .insert(req.body)
+      .insert(data)
       .then(item => utilities.responseHandler(null, res, 201, item))
       .catch(err => utilities.responseHandler(err, res));
   }
@@ -25,10 +78,10 @@ class BaseController {
 
   index(req, res) {
     return findQuery(this.model)
-      .allowEager(this.eager)
       .registerFilter('search', searchFilter)
       .build(req.query.where)
       .skipUndefined()
+      .where(getParameterFilters(req, this.additionalProperties))
       .eager(req.query.include)
       .orderBy(req.query.sort.by, req.query.sort.order)
       .page(req.query.page.number, req.query.page.size)
@@ -39,9 +92,10 @@ class BaseController {
   show(req, res) {
     return this.model.query()
       .findById(req.params[this.id])
-      .allowEager(this.eager)
+      .skipUndefined()
+      .where(getParameterFilters(req, this.additionalProperties))
       .eager(req.query.eager)
-      .then(item => {
+      .then((item) => {
         if (!item) return utilities.throwNotFound(res);
         return utilities.responseHandler(null, res, 200, item);
       })
