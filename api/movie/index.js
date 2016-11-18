0e5020a7e7a0b293a39fea07cd2f2e6baeea61d2
Sayem <afm.sayem@gmail.com>
2016-11-18 18:12:44 +0600

    feat(authentication): add authentication to movie

--- api/movie/index.js
+++ api/movie/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const processQuery = require('../../components/middlewares/process-query');
+const authenticate = require('../../components/middlewares/authenticate');
 const Movie = require('./movie.model');
 const BaseController = require('../base/base.controller');
 const artistRouter = require('./movie_person');
@@ -11,12 +12,14 @@ const router = new express.Router();
 
 router.get('/', processQuery, controller.index.bind(controller));
 router.get('/:movie_id', controller.show.bind(controller));
+
+router.use('/:movie_id/artists', artistRouter);
+router.use('/:movie_id/reviews', reviewRouter);
+
+router.use(authenticate);
 router.post('/', controller.create.bind(controller));
 router.put('/:movie_id', controller.update.bind(controller));
 router.patch('/:movie_id', controller.update.bind(controller));
 router.delete('/:movie_id', controller.destroy.bind(controller));
-
-router.use('/:movie_id/artists', artistRouter);
-router.use('/:movie_id/reviews', reviewRouter);
 
 module.exports = router;
