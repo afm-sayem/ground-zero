498683034d012703ceef5dd7a395bf6bd5c48505
Sayem <afm.sayem@gmail.com>
2016-11-18 12:13:50 +0600

    refactor(user): reuse base class for some methods

--- api/user/index.js
+++ api/user/index.js
@@ -1,17 +1,18 @@
 const express = require('express');
-const controller = require('./user.controller');
 const processQuery = require('../../components/middlewares/process-query');
 const ensureAuthenticated = require('../../components/middlewares/authenticate');
+const UserController = require('./user.controller');
 
 const router = new express.Router();
+const controller = new UserController();
 
 router.use(ensureAuthenticated);
 
-router.get('/me', controller.show);
-router.get('/', processQuery, controller.index);
-router.put('/:id', controller.update);
-router.patch('/:id', controller.update);
-router.delete('/:id', controller.destroy);
-router.post('/unlink', controller.unlink);
+router.get('/me', controller.show.bind(controller));
+router.get('/', processQuery, controller.index.bind(controller));
+router.put('/:user_id', controller.update.bind(controller));
+router.patch('/:user_id', controller.update.bind(controller));
+router.delete('/:user_id', controller.destroy.bind(controller));
+router.post('/unlink', controller.unlink.bind(controller));
 
 module.exports = router;
--- api/user/user.controller.js
+++ api/user/user.controller.js
@@ -1,58 +1,54 @@
 const User = require('./user.model');
+const BaseController = require('../base/base.controller');
 const utilities = require('../../components/utilities');
 const findQuery = require('objection-find');
 
-function index(req, res) {
-  return findQuery(User)
-    .build(req.query.filter)
-    .eager(req.query.include)
-    .omit('hash')
-    .skipUndefined()
-    .orderBy(req.query.sort.by, req.query.sort.order)
-    .page(req.query.page.number, req.query.page.size)
-    .then(users => utilities.responseHandler(null, res, 200, users))
-    .catch(err => utilities.responseHandler(err, res));
-}
-
-function show(req, res) {
-  return User.query()
-    .findById(req.user)
-    .omit('hash')
-    .then(user => {
-      if (!user) return utilities.throwNotFound(res);
-      return utilities.responseHandler(null, res, 200, user);
-    })
-    .catch(err => utilities.responseHandler(err, res));
-}
-
-function update(req, res) {
-  return User.query()
-    .patchAndFetchById(req.params.id, req.body)
-    .then(user => res.send(user));
-}
-
-function destroy(req, res) {
-  return User.query()
-    .delete()
-    .where('id', req.params.id)
-    .then(() => utilities.responseHandler(null, res, 204))
-    .catch(err => utilities.responseHandler(err, res));
-}
-
-function unlink(req, res) {
-  const provider = req.query.provider;
-
-  const providers = ['facebook'];
-  if (providers.indexOf(provider) === -1) {
-    return res.status(404).send('Unknown provider');
+class UserController extends BaseController {
+  constructor() {
+    super(User, 'user_id');
+    this.model = User;
   }
 
-  return User.query()
-    .findById(req.user)
-    .update({ [provider]: null })
-    .then((user) => {
-      res.status(200).send(user);
-    });
+  index(req, res) {
+    return findQuery(this.model)
+      .build(req.query.filter)
+      .eager(req.query.include)
+      .omit('hash')
+      .skipUndefined()
+      .orderBy(req.query.sort.by, req.query.sort.order)
+      .page(req.query.page.number, req.query.page.size)
+      .then(users => utilities.responseHandler(null, res, 200, users))
+      .catch(err => utilities.responseHandler(err, res));
+  }
+
+  show(req, res) {
+    return this.model
+      .query()
+      .findById(req.user)
+      .omit('hash')
+      .then((user) => {
+        if (!user) return utilities.throwNotFound(res);
+        return utilities.responseHandler(null, res, 200, user);
+      })
+      .catch(err => utilities.responseHandler(err, res));
+  }
+
+  unlink(req, res) {
+    const provider = req.query.provider;
+
+    const providers = ['facebook'];
+    if (providers.indexOf(provider) === -1) {
+      return res.status(404).send('Unknown provider');
+    }
+
+    return this.model
+      .query()
+      .findById(req.user)
+      .update({ [provider]: null })
+      .then((user) => {
+        res.status(200).send(user);
+      });
+  }
 }
 
-module.exports = { index, show, update, destroy, unlink };
+module.exports = UserController;
