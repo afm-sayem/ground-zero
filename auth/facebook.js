f0939b046ef1468a28c334252713da71749ec487
Sayem <afm.sayem@gmail.com>
2017-01-12 00:03:36 +0600

    refactor(facebook): facebook authentication should only use access_token

--- auth/facebook.js
+++ auth/facebook.js
@@ -1,63 +1,35 @@
 const request = require('request-promise');
-const jwt = require('jwt-simple');
-
 const User = require('../api/user/user.model');
 const config = require('../config/environment');
 const authUtils = require('./authutils');
 
-function createUser(responseObj, name, facebook, role) {
+function createUser(responseObj, name, facebook) {
   return User.query()
-    .insert({ name, facebook, role })
+    .insert({ name, facebook })
     .then((user) => responseObj.send({ token: authUtils.createJWT(user) }))
     .catch(error => error);
 }
 
 exports.authenticate = function (req, res) {
-  const params = {
-    code: req.body.code,
-    client_id: req.body.clientId,
-    client_secret: config.facebook.secret,
-    redirect_uri: req.body.redirectUri,
-  };
-
-  request.get({ url: config.facebook.oauthUrl, qs: params, json: true }, (err, response, token) => {
-    if (response.statusCode !== 200) {
-      return res.status(500).send({ message: token.error.message });
+  return request.get({
+    url: config.facebook.profile,
+    qs: req.body,
+    json: true,
+  }, (fberr, fbres, profile) => {
+    if (fberr) {
+      res.status(400).send({
+        message: fberr,
+      });
     }
 
-    return request.get({
-      url: config.facebook.graphUrl,
-      qs: token,
-      json: true,
-    }, (reqerr, reqres, profile) => {
-      User.query()
-        .where('facebook', profile.id)
-        .then((resUser) => {
-          if (req.headers.authorization) {
-            if (resUser.length) {
-              res.status(409).send({
-                message: 'There is already a facebook account that belongs to you',
-              });
-            }
-
-            const secretToken = req.headers.authorization.split(' ')[1];
-            const payload = jwt.decode(secretToken, config.token);
-
-            return User.query()
-              .findById(payload.sub)
-              .then((user) => {
-                if (!user) {
-                  return res.status(404).send({ message: 'User not found' });
-                }
-                return createUser(res, profile.name, profile.id, 1);
-              });
-          }
-          if (resUser.length) {
-            return res.status(201).send({ token: authUtils.createJWT(resUser) });
-          }
-          return createUser(res, profile.name, profile.id, 1);
-        });
-    });
+    User.query()
+      .where('facebook', profile.id)
+      .then((user) => {
+        if (user.length) {
+          return res.status(200).send({ token: authUtils.createJWT(user[0]) });
+        }
+        return createUser(res, profile.name, profile.id);
+      });
   });
 };
 
